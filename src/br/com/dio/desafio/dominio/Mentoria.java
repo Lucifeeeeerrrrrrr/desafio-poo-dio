6ffa187485a19f8766440c8900c4642346ae01da
Lucifer <80173371+Lucifeeeeerrrrrrr@users.noreply.github.com>
2025-09-30 08:48:54 -0300

    Update Mentoria.java

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,32 +2,55 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria extends Conteudo{
+public class Mentoria extends Conteudo {
 
+    // Data em que a mentoria será realizada
     private LocalDate data;
 
+    /**
+     * Calcula o XP (experiência) para uma mentoria.
+     * Nesse caso, o valor base é o XP padrão acrescido de 20 pontos.
+     *
+     * @return valor de experiência da mentoria (double)
+     */
     @Override
     public double calcularXp() {
-        return XP_PADRAO + 20d;
+        return XP_PADRAO + 20.0;
     }
 
-    public Mentoria() {
+    // Construtor padrão
+    public Mentoria() { }
+
+    /**
+     * Retorna a data da mentoria.
+     *
+     * @return data em formato LocalDate
+     */
+    public LocalDate getData() { 
+        return data; 
     }
 
-    public LocalDate getData() {
-        return data;
+    /**
+     * Define a data da mentoria.
+     *
+     * @param data objeto LocalDate que representa a data da mentoria
+     */
+    public void setData(LocalDate data) { 
+        this.data = data; 
     }
 
-    public void setData(LocalDate data) {
-        this.data = data;
-    }
-
+    /**
+     * Representação textual da mentoria, exibindo:
+     * título, descrição e data.
+     *
+     * @return string formatada com os dados da mentoria
+     */
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + getTitulo() + '\'' +
-                ", descricao='" + getDescricao() + '\'' +
-                ", data=" + data +
-                '}';
+               "titulo='" + getTitulo() + '\'' +
+               ", descricao='" + getDescricao() + '\'' +
+               ", data=" + data +
+               '}';
     }
 }
